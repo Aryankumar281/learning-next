191c349c64357f9b00ab5f59752684866ba93b6e
AryanKumar281 <kumararyan5214@gmail.com>
2025-10-23 22:47:07 +0530

    learnt some hooks

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,6 +1,19 @@
 "use client";
 
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+
 function Profile(){
-    return <h1>Profile Component - Client</h1>
+    const router = useRouter();
+    const pathName = usePathname();
+    // const searchParams =useSearchParams();
+    // console.log(searchParams)
+    console.log(pathName);
+    const handleNavigate=()=>{
+        router.push('/')
+    }
+    return <div>
+        <h1>Profile Component - Client</h1>
+        <button onClick={handleNavigate} className=" p-2 rounded-xl bg-red-400 transition-all hover:shadow-[10px_5px_5px] shadow-red-600 ">Navigate to home page</button>
+    </div>
 }
 export default Profile;
